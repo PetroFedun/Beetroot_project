66be4a9cd1060a7c63165ab0fae2dcc2e8a84c38
PetroFedun <fedynpetro@gmail.com>
2023-10-06 12:12:25 +0300

    add user login feature

--- bookmark_manager/views.py
+++ bookmark_manager/views.py
@@ -5,6 +5,7 @@ from django.views.generic import UpdateView, DeleteView
 from django.db.models import Q
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def index(request):
     return render(request, 'index.html')
@@ -73,3 +74,21 @@ def register_request(request):
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request, "register.html", {"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("bookmark_list")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request, 'login.html', {"login_form":form})
